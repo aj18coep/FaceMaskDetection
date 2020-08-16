b0ea07b400f81d464ca570eb5ce995a25f44fa95
AshleshaJ <67588914+AshleshaJ@users.noreply.github.com>
2020-08-17 00:04:53 +0530

    Add files via upload

---
+++ Assignment1/A6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int i,j;
+	for(i=2;i<101;i++){
+		for(j=2;j<1;j++){
+			if(i%j==0)
+				break;
+		}
+		if(i==j){
+			printf("%d\n",i);
+		}
+	}
+}
---
+++ Assignment1/ternary.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	a?b:c;
+}
