78bf8e61867998e25781590266ed82d59e451d51
AshleshaJ <67588914+AshleshaJ@users.noreply.github.com>
2020-11-10 11:31:19 +0530

    Add files via upload

---
+++ Assignment2/B1.s
@@ -0,0 +1,47 @@
+	.file	"B1.c"
+	.text
+	.globl	_AddTwo
+	.def	_AddTwo;	.scl	2;	.type	32;	.endef
+_AddTwo:
+LFB13:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	addl	$2, 8(%ebp)
+	movl	8(%ebp), %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE13:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB14:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	call	___main
+	movl	$3, 28(%esp)
+	movl	28(%esp), %eax
+	movl	%eax, (%esp)
+	call	_AddTwo
+	movl	%eax, 28(%esp)
+	movl	28(%esp), %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE14:
+	.ident	"GCC: (MinGW.org GCC Build-2) 9.2.0"
